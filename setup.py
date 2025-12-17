ac4006060d02933df03d600b28a3962a8b4b15b6
Tiago Ribeiro <tiago.ribeiro@noirlab.edu>
2025-12-17 12:14:43 -0700

    Update setup.py stop writting version file.

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 from setuptools import setup
 import setuptools_scm
 
-setup(
-    version=setuptools_scm.get_version(
-        write_to="python/lsst/ts/observatory/model/version.py"
-    )
-)
+setup(version=setuptools_scm.get_version())
